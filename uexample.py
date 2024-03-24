7a9d255562d8305dcab0f9fa3e460311bf501059
Isidro Arias <isidroariass@hotmail.es>
2024-03-24 12:36:02 +0100

    help

--- uexample.py
+++ uexample.py
@@ -34,8 +34,9 @@ colors_256 = {
 }
 
 # green = u.AttrSpec(colors_256["white"], colors_256["green"], 256)
-pool_index = u.AttrSpec(colors_256["green"], 'default', 256)
-new_random =u.AttrSpec('bold', 'default', 256)
+pool_index = u.AttrSpec(colors_256["green"], "default", 256)
+new_random = u.AttrSpec("bold", "default", 256)
+
 
 def main():
 
@@ -45,28 +46,37 @@ def main():
             ("normal", "0x00   \n"),
             (pool_index, "1: "),
             ("normal", "0x0102  <- 0,1\n"),
-        ] * 16
+        ]
+        * 16
     )
     seed_file = u.Text("0x" + "DEADCODE" * 16)
-    output_history = u.Text(["FOCACCIA"*4, (new_random, "FEE1DEAD"*4 )])
-    events = u.Text("Events") # list
-
-    pile = u.Pile(
-        u.LineBox(w, title=t)
-        for w, t in [
-            (seed_file, "seed file"),
-            (output_history, "output history"),
-            (events, "events"),
+    output_history = u.Text(["FOCACCIA" * 4, (new_random, "FEE1DEAD" * 4)])
+    events = u.Text("Events")  # list
+    help = u.Text(
+        [
+            "c: Add entropy from keystrokes char\n",
+            "t: Add entropy from keystrokes timestamp\n",
+            "f: Update seed file\n",
+            "r: generate random\n",
+            "b: edit number of blocks\n",
         ]
     )
-    pile = u.Filler(pile, height=('relative', 100))
 
-    columns = u.Columns(
-           [
-           u.LineBox(pools, title="pools"),
-           pile
-            ]
+    pile = u.Pile(
+        [
+            u.LineBox(w, title=t)
+            for w, t in (
+                (seed_file, "seed file"),
+                (output_history, "output history"),
+                (events, "events"),
+                (help, "help"),
+            )
+        ]
     )
+    print(pile)
+    pile = u.Filler(pile, height=("relative", 100))
+
+    columns = u.Columns([u.LineBox(pools, title="pools"), pile])
 
     filler = u.Filler(columns, "top")
 
@@ -79,16 +89,17 @@ def main():
         elif data == "t":
             seed_file.set_text("updated text")
             return True
-        elif data =='p':
+        elif data == "p":
             from time import sleep
+
             with StoppedScreen(loop.screen):
-                print('hello friend')
+                print("hello friend")
                 sleep(0.5)
-                print('bye friend')
+                print("bye friend")
                 sleep(0.5)
         return False
 
-    loop = u.MainLoop(filler, unhandled_input = unhandled_input)
+    loop = u.MainLoop(filler, unhandled_input=unhandled_input)
 
     loop.run()
 
@@ -96,4 +107,3 @@ def main():
 if __name__ == "__main__":
 
     main()
-
