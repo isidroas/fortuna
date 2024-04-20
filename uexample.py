d0bc65433adb56f9850fcae75c273c18c85a32c6
Isidro Arias <isidroariass@hotmail.es>
2024-04-20 12:59:44 +0200

    logger

--- uexample.py
+++ uexample.py
@@ -1,5 +1,34 @@
 import urwid as u
 
+import logging
+
+LOG = logging.getLogger(__name__)
+
+
+class UrwidHandler(logging.Handler):
+    def __init__(self, events: u.ListBox):
+        self.events = events
+        super().__init__()
+
+    def emit(self, record):
+        add_event(self.events, self.format(record))
+
+    def format(self, record) -> u.Text:
+        if record.levelno is logging.WARNING:
+            color = "yellow"
+        else:
+            color = "green"
+        text = "[%s] %s" % (record.levelname, record.msg)
+        return u.Text((color, text))
+
+
+def add_event(events: u.ListBox, text: u.Text):
+    # source: pudb.debugger.add_cmdline_content
+    events.body.append(text)
+    # esto es necesario porque si no se queda el focus arriba
+    events.set_focus_valign("bottom")
+    events.set_focus(len(events) - 1, coming_from="above")
+
 
 # using high colors file:///home/isidro/op/urwid/build/documentation/manual/displayattributes.html#bright-background-colors instead of bright I don't remember why
 colors_256 = {
@@ -24,22 +53,30 @@ colors_256 = {
 # green = u.AttrSpec(colors_256["white"], colors_256["green"], 256)
 pool_index = u.AttrSpec(colors_256["green"], "default", 256)
 new_random = u.AttrSpec("bold", "default", 256)
-old_random = u.AttrSpec(colors_256['light gray'], "default", 256)
+old_random = u.AttrSpec(colors_256["light gray"], "default", 256)
 
-def create_top(pools: u.Text, seed_file: u.Text, output_history: u.Text, events: u.SimpleListWalker, help: u.Text):
+
+def create_top(
+    pools: u.Text,
+    seed_file: u.Text,
+    output_history: u.Text,
+    events: u.SimpleListWalker,
+    help: u.Text,
+):
     pile = u.Pile(
         [
             u.Columns(
                 [
-                    u.LineBox(u.Filler(seed_file, 'top'), title="seed file"),
-                    u.LineBox(u.Filler(output_history, 'bottom'), title="output history"),
+                    u.LineBox(u.Filler(seed_file, "top"), title="seed file"),
+                    u.LineBox(
+                        u.Filler(output_history, "bottom"), title="output history"
+                    ),
                 ]
             ),
             u.Columns(
                 [
                     u.LineBox(events, title="events"),
-                    u.LineBox(u.Filler(help, 'bottom'), title="help"),
-
+                    u.LineBox(u.Filler(help, "bottom"), title="help"),
                 ]
             ),
         ]
@@ -48,10 +85,12 @@ def create_top(pools: u.Text, seed_file: u.Text, output_history: u.Text, events:
     top = u.Columns([u.LineBox(u.Filler(pools), title="pools"), pile])
     return top
 
+
 def add_output_history(output_history: u.Text, data: bytes):
     # TODO: u.Text no se puede modificar, ni u.Edit. Usar también ListBox?
     output_history[-1] = (old_random, output_history[-1][1])
     output_history.append((new_random, data.hex().upper()))
+
 
 def main():
 
@@ -65,17 +104,25 @@ def main():
         * 16
     )
     seed_file = u.Text("0x" + "DEADCODE" * 16)
-    output_history = u.Text([(old_random, "FOCACCIA" * 4), (new_random, "FEE1DEAD" * 4)])
+    output_history = u.Text(
+        [(old_random, "FOCACCIA" * 4), (new_random, "FEE1DEAD" * 4)]
+    )
 
-    # events = u.SimpleListWalker([])
-    events = u.ListBox(u.SimpleListWalker(u.Text(t) for t in [
-        "Pressed 'k'",
-        "Pressed 'j'",
-        "timestamp: 16:59:30.671210",
-        "timestamp: 16:59:30.952142",
-        "not writing seed file",
-        "found empty tmp/seed_file file",
-    ]))
+    events = u.ListBox(
+        u.SimpleListWalker(
+            u.Text(t)
+            for t in [
+                "Pressed 'k'",
+                "Pressed 'j'",
+                "timestamp: 16:59:30.671210",
+                "timestamp: 16:59:30.952142",
+                "not writing seed file",
+                "found empty tmp/seed_file file",
+            ]
+        )
+    )
+
+    LOG.addHandler(UrwidHandler(events))
 
     help = u.Text(
         [
@@ -87,7 +134,6 @@ def main():
         ]
     )
 
-
     def unhandled_input(data: str | tuple[str, int, int, int]) -> bool | None:
         # print(data)
         if data == "q":
@@ -95,18 +141,21 @@ def main():
         elif data == "t":
             seed_file.set_text("updated text")
             return True
+        # elif data == "e":
+        #     add_event(events, u.Text('alarmmm!'))
+        elif data == "w":
+            LOG.warning("esto petardea :)")
         elif data == "e":
-            # source: pudb.debugger.add_cmdline_content
-            events.body.append(u.Text('alarmmm!'))
-            # esto es necesario porque si no se queda el focus arriba
-            events.set_focus_valign("bottom")
-            events.set_focus(len(events) - 1, coming_from="above")
+            LOG.error("esto petardea :)")
         elif data == "r":
-            add_output_history(output_history, b'\xde\xca\xfb\xad')
+            add_output_history(output_history, b"\xde\xca\xfb\xad")
 
         return False
 
-    loop = u.MainLoop(create_top(pools, seed_file, output_history, events, help) , unhandled_input=unhandled_input)
+    loop = u.MainLoop(
+        create_top(pools, seed_file, output_history, events, help),
+        unhandled_input=unhandled_input,
+    )
     loop.run()
 
 
