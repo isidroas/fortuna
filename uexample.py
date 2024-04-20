ccda3f7ce26afb599c98f747b18c53d454fc9032
Isidro Arias <isidroariass@hotmail.es>
2024-04-20 11:53:36 +0200

    refacotr

--- uexample.py
+++ uexample.py
@@ -24,9 +24,9 @@ colors_256 = {
 # green = u.AttrSpec(colors_256["white"], colors_256["green"], 256)
 pool_index = u.AttrSpec(colors_256["green"], "default", 256)
 new_random = u.AttrSpec("bold", "default", 256)
+old_random = u.AttrSpec(colors_256['light gray'], "default", 256)
 
 def create_top(pools: u.Text, seed_file: u.Text, output_history: u.Text, events: u.SimpleListWalker, help: u.Text):
-    events_list =u.ListBox(events) # only used once?
     pile = u.Pile(
         [
             u.Columns(
@@ -37,7 +37,7 @@ def create_top(pools: u.Text, seed_file: u.Text, output_history: u.Text, events:
             ),
             u.Columns(
                 [
-                    u.LineBox(events_list, title="events"),
+                    u.LineBox(events, title="events"),
                     u.LineBox(u.Filler(help, 'bottom'), title="help"),
 
                 ]
@@ -47,6 +47,11 @@ def create_top(pools: u.Text, seed_file: u.Text, output_history: u.Text, events:
 
     top = u.Columns([u.LineBox(u.Filler(pools), title="pools"), pile])
     return top
+
+def add_output_history(output_history: u.Text, data: bytes):
+    # TODO: u.Text no se puede modificar, ni u.Edit. Usar también ListBox?
+    output_history[-1] = (old_random, output_history[-1][1])
+    output_history.append((new_random, data.hex().upper()))
 
 def main():
 
@@ -60,17 +65,17 @@ def main():
         * 16
     )
     seed_file = u.Text("0x" + "DEADCODE" * 16)
-    output_history = u.Text(["FOCACCIA" * 4, (new_random, "FEE1DEAD" * 4)])
+    output_history = u.Text([(old_random, "FOCACCIA" * 4), (new_random, "FEE1DEAD" * 4)])
 
     # events = u.SimpleListWalker([])
-    events = u.SimpleListWalker(u.Text(t) for t in [
+    events = u.ListBox(u.SimpleListWalker(u.Text(t) for t in [
         "Pressed 'k'",
         "Pressed 'j'",
         "timestamp: 16:59:30.671210",
         "timestamp: 16:59:30.952142",
         "not writing seed file",
         "found empty tmp/seed_file file",
-    ])  # list
+    ]))
 
     help = u.Text(
         [
@@ -92,9 +97,12 @@ def main():
             return True
         elif data == "e":
             # source: pudb.debugger.add_cmdline_content
-            events.append(u.Text('alarmmm!'))
-            events_list.set_focus_valign("bottom")
-            events_list.set_focus(len(events) - 1, coming_from="above")
+            events.body.append(u.Text('alarmmm!'))
+            # esto es necesario porque si no se queda el focus arriba
+            events.set_focus_valign("bottom")
+            events.set_focus(len(events) - 1, coming_from="above")
+        elif data == "r":
+            add_output_history(output_history, b'\xde\xca\xfb\xad')
 
         return False
 
